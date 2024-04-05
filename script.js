aa1f62e6a2737a28b7856fd3b961edd09cc72073
Anna Lukaszczyk <normalnie4@tlen.pl>
2024-04-05 15:55:19 +0200

    toggle task done

--- script.js
+++ script.js
@@ -23,12 +23,18 @@
 		render();
 	};
 
+const toggleTaskDone = (taskIndex) => {
+	tasks[taskIndex].done = !tasks[taskIndex].done;
+	render();
+}
+
 	const render = () => {
 		let htmlString = "";
 
 		for (const task of tasks) {
 			htmlString += `
         <li${task.done ? ' style="text-decoration: line-through"' : ""}>
+		<button class="js-done">zrobione?</button>
 		<button class="js-remove">usuń</button>
         ${task.content}
         </li>
@@ -42,6 +48,14 @@
 		removeButtons.forEach((removeButton, index) => {
 			removeButton.addEventListener("click", () => {
 				removeTask(index);
+			});
+		});
+
+		const toggleDoneButtons = document.querySelectorAll(".js-done");
+
+		toggleDoneButtons.forEach((toggleDoneButton, index) => {
+			toggleDoneButton.addEventListener("click", () => {
+				toggleTaskDone(index);
 			});
 		});
 	};
